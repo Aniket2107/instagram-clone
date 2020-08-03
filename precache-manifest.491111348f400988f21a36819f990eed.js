8e02a9370d8b5bad69e3b698385993cc3ecaa80d
Aniket2107 <had096705@gmail.com>
2020-08-04 01:05:10 +0530

    Updates

---
+++ precache-manifest.491111348f400988f21a36819f990eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2137dafadd361ae6418b8975882ff6a9",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "4d9210d91126e2e63eff",
+    "url": "/instagram-clone/static/css/main.c2065533.chunk.css"
+  },
+  {
+    "revision": "50e5cf679c3766ca71e8",
+    "url": "/instagram-clone/static/js/2.606cdbe5.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instagram-clone/static/js/2.606cdbe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d9210d91126e2e63eff",
+    "url": "/instagram-clone/static/js/main.4cd971f0.chunk.js"
+  },
+  {
+    "revision": "9d73230a1dae251b7590",
+    "url": "/instagram-clone/static/js/runtime-main.8aef135a.js"
+  }
+]);
